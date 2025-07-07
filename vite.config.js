3a84469e6f3189e5b9885e676c43b7a0f346c8fb
joacx200222 <150563155+joacx200222@users.noreply.github.com>
2025-07-06 22:50:04 -0500

    otra

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/adopcion-perritos/',
+  base: '/adopcion-perritos/', // 👈 IMPORTANTE: esto es lo que arregla el problema
   plugins: [react()],
 })
