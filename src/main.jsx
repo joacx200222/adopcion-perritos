ba4e8183eb43d5019282ec7c8b3f2deb85144f11
joacx200222 <150563155+joacx200222@users.noreply.github.com>
2025-07-06 23:02:22 -0500

    una mas

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
-import { HashRouter } from 'react-router-dom'; // 💡 Importa HashRouter
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles.css'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </HashRouter>
   </React.StrictMode>
-);
+)
