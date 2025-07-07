7c400365257b163f1391985a1368b3be83b96390
joacx200222 <150563155+joacx200222@users.noreply.github.com>
2025-07-06 22:37:06 -0500

    TODO

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/adopcion-perritos/', // 👈 tu nombre de repo exacto
   plugins: [react()],
 })
